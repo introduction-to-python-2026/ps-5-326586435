043ef75a5538fec80150b17a14c9fb361b7f4226
326586435 <nourali0307@gmail.com>
2025-11-27 00:33:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import string_utils as su, equation_utils as eu
+import string_utils as su
+import equation_utils as eu
 
-def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
+def balance_reaction(reaction):
 
-    # 1.parse reaction
-    reactants, products == su.parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
-    reactant_atoms = su.count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
-    product_atoms = su.count_atoms_in_reaction(products)
+    reactants, products = su.parse_chemical_reaction(reaction)
 
-    # 2.build equation and solve
+    reactant_atoms = su.count_atoms_in_reaction(reactants)
+    product_atoms  = su.count_atoms_in_reaction(products)
+
     equations, coefficients = eu.build_equations(reactant_atoms, product_atoms)
+
     coefficients = eu.my_solve(equations, coefficients) + [1]
 
-    return coefficients # [1/3, 1, 2/3, 1]
+    return coefficients
+
+
 
